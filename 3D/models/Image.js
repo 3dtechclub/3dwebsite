1a68fd074ac949c29f385658b607d7a4ccd1b45e
3dtechclub <83449017+3dtechclub@users.noreply.github.com>
2021-05-02 18:37:22 +0530

    Delete 3D directory

--- 3D/models/Image.js
+++
@@ -1,8 +0,0 @@
-var mongoose=require("mongoose");
-var albumSchema=new mongoose.Schema({
-    name:String,
-    images:[String],
-    count:Number,
-    dateCreated:Date
-});
-module.exports=mongoose.model("Image",albumSchema);
